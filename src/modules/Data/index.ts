46b7515964a0f043267856ba2090b1a9cd72f163
Justin C Kirk <justin.c.kirk@gmail.com>
2023-03-15 14:34:15 -0400

    adds photos styles

--- src/modules/Data/index.ts
+++ src/modules/Data/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const projectsURL = 'https://raw.githubusercontent.com/intern-jck/site-html/main/assets/data/mechaData.json';
+const photosURL = 'https://raw.githubusercontent.com/intern-jck/site-html/main/assets/data/photosData.json';
+const codeURL = 'https://raw.githubusercontent.com/intern-jck/site-html/main/assets/data/codeData.json';
 
 export async function getProjects() {
   try {
@@ -9,6 +11,17 @@ export async function getProjects() {
     return projects;
   } catch (error) {
     console.log('get projects error:', error)
+    return error;
+  }
+}
+
+export async function getPhotos() {
+  try {
+    const response = await axios.get(photosURL);
+    const photos = await response.data;
+    return photos;
+  } catch (error) {
+    console.log('get photos error:', error)
     return error;
   }
 }
--- src/pages/photos/index.tsx
+++ src/pages/photos/index.tsx
@@ -1,14 +1,52 @@
 import styles from '@/styles/Photos.module.scss';
+import { getPhotos } from '../../modules/Data';
+import Image from 'next/image'
 
-// const inter = Inter({ subsets: ['latin'] })
+type Props = {
+  photos: Array<string>,
+};
 
-// import Image from 'next/image'
-export default function Photos() {
+export default function Photos({ photos }: Props) {
   return (
-    <>
-      <main className={styles.main}>
-        PHOTOS PAGE
-      </main>
-    </>
+    <div className='container'>
+
+      <div className='page-header'>
+        <h1>PHOTOGRAPHY</h1>
+      </div>
+
+      <div className='page-content'>
+        <div className={styles.photosContent}>
+          {
+            photos.map((photo, i) => {
+              return (
+                <button key={i} className={styles.photoButton}>
+                  <Image
+                    loading='lazy'
+                    src={photo}
+                    alt='photo not found...'
+                    width='600'
+                    height='400'
+                  // fill
+                  // sizes='(max-width: 768px) 100vw,
+                  // (max-width: 1200px) 50vw,
+                  // 33vw'
+                  />
+                </button>
+              )
+
+            })
+          }
+        </div>
+      </div>
+
+    </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const photos = await getPhotos();
+  return {
+    props: { photos }
+  }
 }

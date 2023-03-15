047c27121b871696fc480a4315bb574d364fc5a6
intern laptop <justin.c.kirk@gmail.com>
2023-03-15 09:20:51 -0400

    adds config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'iili.io',
+        port: '',
+      },
+    ],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "variables.scss";`
+  }
 }
 
 module.exports = nextConfig

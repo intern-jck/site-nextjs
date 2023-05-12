5c591d530b1e1be252df64d6f405f146b7259883
Justin C Kirk <justin.c.kirk@gmail.com>
2023-05-12 16:56:17 -0400

    adds hamburger menu styles

--- src/common/components/Navbar/index.tsx
+++ src/common/components/Navbar/index.tsx
@@ -1,44 +1,52 @@
 import Link from 'next/link';
 import styles from './Navbar.module.scss'
 
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 export default function NavBarComponent() {
+
+  function toggleMenu(event: React.MouseEvent<HTMLButtonElement>) {
+    const navMenu = document.getElementById(styles.navMenu);
+    navMenu ?  navMenu.classList.toggle(styles.showMenu) : null;
+  };
+
+  function closeMenu(event: React.MouseEvent<HTMLAnchorElement>) {
+    const navMenu = document.getElementById(styles.navMenu);
+    navMenu ?  navMenu.classList.remove(styles.showMenu) : null;
+  }
+
   return (
-
     <nav className={styles.navbar}>
-      <div className={styles.brand}>
-        <Link href='/'>
-          JCK
-        </Link>
-      </div>
-
-      <div className={styles.navMenu}>
-        <ul>
-          <li>
-            <Link href='/work'>
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link href='/photos'>
-              Photos
-            </Link>
-          </li>
-          <li>
-            <Link href='/randd'>
-              R&D
-            </Link>
-          </li>
-          <li>
-            <Link href='/contact'>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
 
       <div className={styles.hamMenu}>
-        HAM
+        <button onClick={toggleMenu}>
+          <GiHamburgerMenu />
+        </button>
       </div>
+
+      <ul id={styles.navMenu} className={styles.navMenu}>
+        <li>
+          <Link href='/work' onClick={closeMenu}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link href='/photos' onClick={closeMenu}>
+            Photos
+          </Link>
+        </li>
+        <li>
+          <Link href='/randd' onClick={closeMenu}>
+            R&D
+          </Link>
+        </li>
+        <li>
+          <Link href='/contact' onClick={closeMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
     </nav>
   )
 };
--- src/modules/json/getJsonDataFromUrl.ts
+++ src/modules/json/getJsonDataFromUrl.ts
@@ -4,7 +4,6 @@ export async function getJsonDataFromUrl(url:string) {
   try {
     const response = await axios.get(url);
     const data = await response.data;
-    // console.log(data)
     return data;
   } catch (error) {
     console.log('getJsonData Error:', error)
